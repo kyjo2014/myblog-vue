c9d751bb353556d734a2b0d3348eefae22ef2e13
kyjo2014 <467346087@qq.com>
2017-03-18 00:21:36 +0800

    尝试使用async

--- server_side/app.js
+++ server_side/app.js
@@ -1,10 +1,22 @@
 const koa = require('koa')
+const router = require('koa-router')
 
 var app = new koa();
+const route = new router()
+function timeout(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-app.use(function *(){
-  this.body = 'Hello World';
-  console.log(this)
-});
+route.get('/', async function (ctx) {
+  await timeout(200)
+  ctx.body = "Hello world 3";
+})
+app.use(route.routes())
+// app.use(function *(){
+//   this.body = 'Hello World';
+//   console.log(this)
+// });
 
 app.listen(3000);
