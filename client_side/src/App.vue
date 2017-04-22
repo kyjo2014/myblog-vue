f4d3090608e2f682511ab0c29368d91bb1627fac
kyjo2014 <467346087@qq.com>
2017-04-22 23:35:37 +0800

    增加navbar和toaster

--- client_side/src/App.vue
+++ client_side/src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <router-view></router-view>
-  
+    <Aside></Aside>
+    <Toaste></Toaste>
   </div>
 </template>
 
 <script>
+  import Navbar from './components/navbar'
+  export default {
+    name: 'app',
+    components: {
+      Navbar
+    }
+  }
 
-export default {
-  name: 'app'
-
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
 </style>
