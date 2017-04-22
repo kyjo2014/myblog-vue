fcda9d7e6fd06ed6c0268a3e9a1ea76ac937a6dc
kyjo2014 <467346087@qq.com>
2017-04-22 23:35:20 +0800

    修改navbar的选项名字

---
+++ client_side/src/components/navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <mu-tabs :value="activeTab" @change="handleTabChange">
+      <mu-tab value="tab1" title="首页" href="/#/index"/>
+      <mu-tab value="tab3" title="登录" href="/#/login"/>
+    </mu-tabs>
+    <div v-if="activeTab === 'tab1'">
+      <h2>Tab One</h2>
+      <p>
+        这是第一个 tab
+      </p>
+    </div>
+    <div v-if="activeTab === 'tab2'">
+      <h2>Tab Two</h2>
+      <p>
+        这是第二个 tab
+      </p>
+    </div>
+    <div v-if="activeTab === 'tab3'">
+      <h2>Tab Three</h2>
+      <p>
+        这是第三个 tab
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeTab: 'tab1'
+      }
+    },
+    methods: {
+      handleTabChange(val) {
+        this.activeTab = val
+      },
+      handleActive() {
+        window.alert('tab active')
+      }
+    }
+  }
+
+</script>
+<style lang='stylus'>
+
+
+</style>
