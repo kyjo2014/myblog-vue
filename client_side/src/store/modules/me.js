638f4e88cec1c8e476bbe2ba03e3157e68f3d1de
kyjo2014 <467346087@qq.com>
2017-08-02 00:11:58 +0800

    add user redux

--- client_side/src/store/modules/me.js
+++ client_side/src/store/modules/me.js
@@ -1,48 +1,50 @@
 import axios from '../../util//modifiedAxios'
 export default {
-    state : {
-        icon: '',
-        name: '',
-        info: {
+  state: {
+    icon: '',
+    name: '',
+    info: {
 
-        }
-    },
-    getters : {
-        posts: state => state.posts,
-        pageIdx: state => state.pageIdx,
-        total: state => state.total
-    },
-    mutations : {
-        updatePosts(state, posts) {
-            Object.assign(state, {
-                posts: {
-                    [posts.pageIdx]: posts.posts
-                },
-                total: posts.total
-            })
-        },
-        changePage(state, pageIdx) {
-            Object.assign(state, {pageIdx})
-        }
-    },
-    actions : {
-        loadPage({
-            commit,
-            state
-        }, pageIdx) {
-            !state.posts[pageIdx] && axios
-                .get('./posts', {params: {
-                    pageIdx
-                }})
-                .then((res) => {
-                    res.data.code == '200' && commit('updatePosts', {
-                        pageIdx,
-                        posts: res.data.data.posts,
-                        total: res.data.data.Pagination.totalPage
-                    })
-                }, (err) => {
-                    alert(err)
-                })
-        }
     }
+  },
+  getters: {
+    user: state => state
+  },
+  mutations: {
+    updateUser(state, user) {
+      let {
+        icon,
+        name,
+        info
+      } = user
+      Object.assign(state, {
+        icon,
+        name,
+        info
+      })
+    }
+
+  },
+  actions: {
+    fetchUser({
+      commit,
+      state
+    }) {
+      !state.posts[pageIdx] && axios
+        .get('./posts', {
+          params: {
+            pageIdx
+          }
+        })
+        .then((res) => {
+          res.data.code == '200' && commit('updatePosts', {
+            pageIdx,
+            posts: res.data.data.posts,
+            total: res.data.data.Pagination.totalPage
+          })
+        }, (err) => {
+          alert(err)
+        })
+    }
+  }
 }
