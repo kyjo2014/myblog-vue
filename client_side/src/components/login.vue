fb6d4cc7a8f25a890cd389c63fa89c7f90edf91f
kyjo2014 <467346087@qq.com>
2017-08-06 23:19:22 +0800

    完成登录框布局

--- client_side/src/components/login.vue
+++ client_side/src/components/login.vue
@@ -1,18 +1,22 @@
 <template>
-    <mu-dialog id="login-popup" :open="show" title="Dialog" @close="close">
-        <div class="guest" v-if="isGuest">
+    <mu-dialog dialogClass="login-popup" :open="show" title="登录" @close="close">
+        <div class="input guest" v-if="isGuest">
             <mu-text-field name="id" v-model="guestInfo.nickName" hintText="你的昵称" />
+            <br/>
             <mu-text-field name="value" v-model="guestInfo.email" hintText="你的邮箱" />
         </div>
-        <div class="host" v-else>
+        <div class="input host" v-else>
             <mu-text-field name="id" v-model="hostInfo.id" hintText="你的用户名" />
-            <mu-text-field name="value" v-model="hostInfo.pwd" hintText="你的密码" />
+            <mu-text-field name="value" v-model="hostInfo.password" hintText="你的密码" />
         </div>
         <div class="role-sel">
             <mu-radio label="游客" name="role" nativeValue="guest" v-model="role" class="demo-radio" />
             <mu-radio label="管理员" name="role" nativeValue="host" v-model="role" class="demo-radio" />
+            <br/> 游客可直接登录
         </div>
-        <mu-flat-button label="登录" class="demo-flat-button" @click="login" primary/>
+        <div class="login-ctrl">
+            <mu-flat-button label="登录" class="login-btn" @click="login" primary/>
+        </div>
     </mu-dialog>
 </template>
 <script>
@@ -26,12 +30,12 @@ export default {
             },
             hostInfo: {
                 id: '',
-                pwd: ''
+                password: ''
             },
         }
     },
     watch: {
-        show (a) {
+        show(a) {
             console.log(a)
         }
     },
@@ -76,6 +80,20 @@ export default {
     }
 }
 </script>
-<style lang="stylus">
-    
+<style>
+.login-popup {
+    width: 460px;
+    padding: 10px;
+}
+</style>
+
+<style scoped>
+.input,
+.role-sel {
+    text-align: center;
+}
+
+.login-ctrl {
+    text-align: center;
+}
 </style>
